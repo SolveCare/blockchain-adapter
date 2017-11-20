ac8183c17c9cbf6bc4dd5c4b1ba38f66221856ab
nikita.tarasenko <nikita.tarasenko@aidditive.com>
2017-11-20 22:40:32 +0200

    remove explicit dependency on `fabric-sdk-java`

--- src/main/java/care/solve/blockchain/BlockchainAdapterApp.java
+++ src/main/java/care/solve/blockchain/BlockchainAdapterApp.java
@@ -1,20 +1,9 @@
 package care.solve.blockchain;
 
-import care.solve.fabric.FabricSdk;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.TypeExcludeFilter;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {
-        BlockchainAdapterApp.class, FabricSdk.class
-},
-        excludeFilters = {
-                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
-                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
 public class BlockchainAdapterApp {
     public static void main( String[] args ) {
         SpringApplication.run(BlockchainAdapterApp.class, args);
